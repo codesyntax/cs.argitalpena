7690783c7cc1bea4e262886c90e522216b3632de
libargutxi <libargutxi@gmail.com>
2009-02-05 15:31:35 +0000

    argitalpen produktua sortu
    svn path=/cs.argitalpena/trunk/; revision=17998

---
+++ cs/argitalpena/config.py
@@ -0,0 +1,8 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'cs.argitalpena'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+}
---
+++ cs/argitalpena/interfaces.py
@@ -0,0 +1,9 @@
+from zope import schema
+from zope.interface import Interface
+
+from zope.app.container.constraints import contains
+from zope.app.container.constraints import containers
+
+from cs.argitalpena import argitalpenaMessageFactory as _
+
+# -*- extra stuff goes here -*-
