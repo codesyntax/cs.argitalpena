96604e2dae2030fa1b132b252b824ef158c9730a
Lur Ibargutxi <libargutxi@codesyntax.com>
2014-02-04 11:40:29 +0100

    Plone4.3.2 version

--- cs/argitalpena/interfaces.py
+++ cs/argitalpena/interfaces.py
@@ -1,30 +1,8 @@
 from zope import schema
 from zope.interface import Interface
 
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
-
-from cs.argitalpena import argitalpenaMessageFactory as _
 
 # -*- extra stuff goes here -*-
 class Iargitalpena(Interface):
     """Description of the Example Type"""
-    
-
-
-    title = schema.TextLine(title=_(u'Name'),
-                            required=True,
-                            )
-			    
-    description = schema.TextLine(title=_(u'Description'),
-                                  description=_(u'A short summary of this product'),
-                                  )
-				  
-    file = schema.TextLine(title=_(u'File'),
-                                  description=_(u'File'),
-                                  )
-
-    image = schema.TextLine(title=_(u'Image'),
-                                  description=_(u'Image'),
-                                  )
 				  
